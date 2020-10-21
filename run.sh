a2b95cfd5372053afe300ea720b6e7c767932c71
yassin slati <sltyassin@gmail.com>
2020-10-21 14:46:21 +0100

    add mysql

--- run.sh
+++ run.sh
@@ -8,13 +8,12 @@ fi
 
 OUTPUT=$(docker-machine ls | awk '{print $1}' | grep default)
 if [[ $OUTPUT != 'default' ]]; then # we will add more complex testing like docker-machine status
-    echo "Start creating virtualBox ..."
+    echo -e "\033[0;31mStart creating virtualBox"
     docker-machine create --driver virtualbox default > /dev/null
-    echo "VirtualBox created "
+    echo -e "\033[1;33mVirtualBox created"
     docker-machine env default
     eval $(docker-machine env default)
-    echo "Done"
-    eval $(docker-machine env default)
+    echo -e "\033[0;32mDone"
     docker-machine start
 fi
 #
