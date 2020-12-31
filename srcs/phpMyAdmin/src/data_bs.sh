5b7427b7c5dcf58733e9ca86027b01bf765fac23
yassin slati <sltyassin@gmail.com>
2020-12-31 18:31:36 +0100

    add yaml files

--- srcs/phpMyAdmin/src/data_bs.sh
+++ srcs/phpMyAdmin/src/data_bs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "create user admin@localhost identified by 'admin1234';" | mysql -u root
+echo "create user admin@localhost identified by 'admin';" | mysql -u root
 echo "grant all privileges on *.* to admin@localhost;" | mysql -u root
 echo "flush privileges;" | mysql -u root
 echo "quit" | mysql -u root
