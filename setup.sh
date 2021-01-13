05322f9c07db08eabecc9977aeaf52ba0efe1625
yassin slati <sltyassin@gmail.com>
2021-01-13 12:59:52 +0100

    awdii

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ echo -e "\033[1;31m######## minikube Deleted ########\033[0m"
 minikube start --vm-driver=virtualbox --memory=4g --disk-size=20g --cpus=4
 echo -e "\033[33m######## minikube started ########\033[0m"
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f metallb.yaml
--- srcs/MySQL/src/script.sh
+++ srcs/MySQL/src/script.sh
@@ -1,15 +1,15 @@
 if [ ! -f "/var/lib/mysql/ib_buffer_pool" ];
 then
-    /etc/init.d/mariadb setup &> /dev/null
-    service mariadb restart &> null
+    /etc/init.d/mariadb setup &>/dev/null
+    service mariadb restart &>/dev/null
     echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';" | mysql -u root
 	echo "CREATE DATABASE wordpress;" | mysql -u root
 	echo "GRANT ALL PRIVILEGES on *.* to 'admin'@'%' IDENTIFIED BY 'admin';" | mysql -u root
 	echo "FLUSH PRIVILEGES;" | mysql -u root
 	mysql -u root < wordpress.sql
+	mysql -u root < users_data.sql
 fi
 sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
 service mariadb restart
-# /usr/bin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --user=root
-
-tail -F /dev/null
+/usr/bin/supervisord
+#tail -f /dev/null
