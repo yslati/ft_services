0e0187075ffe38bbb7f223c3c3aa4168b7fa2afe
yassin slati <sltyassin@gmail.com>
2021-01-18 10:11:17 +0100

    DONE (y)

--- srcs/MySQL/test_mysql.js
+++ srcs/MySQL/test_mysql.js
@@ -1,17 +1,23 @@
-const mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "192.168.99.118",
-  user: "admin",
-  password: "admin"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  
-});
-
-
+// This script to test if you can connect to Your MySQL outside localhost
+//
+// Run those commands first:
 // brew install node
 // npm i mysql
+//
+// then replace " host, user, password " with your Mysql info
+// run the script and check if the output is Connected or not.
+
+
+const mysql = require('mysql');
+  2
+  3 var con = mysql.createConnection({
+  4   host: "machine_IP",
+  5   user: "Mysql_username",
+  6   password: "MySQL_Password"
+  7 });
+  8
+  9 con.connect(function(err) {
+ 10   if (err) throw err;
+ 11   console.log("Connected!");
+ 12
+ 13 });
